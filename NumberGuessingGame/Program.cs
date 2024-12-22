0680eb5e24a56bfc626cb91a6267712f51ce1034
Leland <leland.warner@walmart.com>
2024-12-21 19:07:13 -0500

    add the core of the game

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+Console.WriteLine("Welcome to the Number Guessing Game!");
+Console.WriteLine("Please enter your name:");
+string userName = Console.ReadLine();
+Console.WriteLine($"{userName}, I'm thinking of a number between 1 and 50. Try to guess it!");
+int numberToGuess = new Random().Next(1, 51);
+int numberOfTries = 5;
+while (numberOfTries > 0)
+{
+    Console.WriteLine($"You have {numberOfTries} tries left. Enter your guess:");
+    bool validGuess = int.TryParse(Console.ReadLine(), out int userGuess);
+    if (userGuess < 1 || userGuess > 50)
+    {
+        Console.WriteLine("Please enter a number between 1 and 50.");
+        continue;
+    }
+    if (validGuess == false)
+    {
+        Console.WriteLine("Please enter a valid number.");
+        continue;
+    }
+    if (userGuess == numberToGuess)
+    {
+        Console.WriteLine($"Congratulations, {userName}! You guessed the correct number: {numberToGuess}");
+        return;
+    }
+    else if (userGuess < numberToGuess)
+    {
+        Console.WriteLine("Too low! Try again.");
+    }
+    else
+    {
+        Console.WriteLine("Too high! Try again.");
+    }
+    numberOfTries--;
+}
+Console.WriteLine($"Sorry, {userName}. You ran out of tries. The correct number was: {numberToGuess}");
+Console.WriteLine("Game over! Press any key to exit.");
+Console.ReadKey();
--- NumberGuessingGame/obj/Debug/net9.0/NumberGuessingGame.AssemblyInfo.cs
+++ NumberGuessingGame/obj/Debug/net9.0/NumberGuessingGame.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e8500221d4e7c5a22dcb998ea8a609c27a4b27")]
 [assembly: System.Reflection.AssemblyProductAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NumberGuessingGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
